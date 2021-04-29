03912f6837bf0dd25c43aba8792bfc401628dba3
Diego Bonilla <drbv27@gmail.com>
2021-04-29 13:08:49 -0500

    Restructuring the project

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,40 +1,6 @@
-/* var catAndActs = {};
-catAndActs['Classroom Instruction and Assessment'] = ['Assessment Day', 'Common Assessment Development', 'Data Team', 'Kindergarten Screening', 'Other'];
-catAndActs['Curriculum Development and Alignment'] = ['Capstone Development', 'Course Of Study Development / Revision', 'Standards Alignment / Rollout', 'Other'];
-catAndActs['District Committee'] = ['Curriculum Council', 'Grading & Assessment Task Force', 'Professional Development Planning Committee', 'Race To The Top Committee', 'Teacher Evaluation Committee', 'Other'];
-catAndActs['Meeting'] = ['Academic Support Team', 'ELL / eKLIP Teachers', 'Gifted Intervention Specialist', 'Intervention Assistance Team', 'Intervention Teachers', 'Kindergarten Parent Conference', 'KLIP Teachers', 'Title I Teachers', 'Other'];
-catAndActs['Other Category'] = ['Other'];
-catAndActs['Professional Conference'] = ['Conference'];
-catAndActs['Professional Workshop / Training'] = ['In-District', 'Out-Of-District'];
-catAndActs['Pupil Services'] = ['IEP Meeting', 'IEP Writing'];
-
-function ChangecatList() {
-    var catList = document.getElementById("validationCustom03");
-    var actList = document.getElementById("validationCustom04");
-    var selCat = catList.options[catList.selectedIndex].value;
-    while (actList.options.length) {
-        actList.remove(0);
-    }
-    var cats = catAndActs[selCat];
-    if (cats) {
-        var i;
-        for (i = 0; i < cats.length; i++) {
-            var cat = new Option(cats[i], i);
-            actList.options.add(cat);
-        }
-    }
-}  */
-
-
 const container = document.querySelector('.container');
 const resultado = document.querySelector('#resultadin');
 const formulario = document.querySelector('#formulario');
-
-
-/* window.addEventListener('load', () => {
-    formulario.addEventListener('submit', mostrarOpcion);
-} ) */
-
 
 function mostrarOpcion(e) {
     e.preventDefault();
@@ -147,62 +113,6 @@ function menuC() {
   divcursos.appendChild(dcseN);
   console.log(divcursos)
   resultadin.appendChild(divcursos);
-    
-    
-    
-    /* dropdown1.classList.add('dropdown','mx-auto');
-
-    const curso = document.createElement('button');
-    curso.textContent = 'Curso';
-    curso.setAttribute("data-toggle", "dropdown")
-
-    curso.classList.add('btn', 'btn-primary', 'dropdown-toggle');
-
-    const menu1 = document.createElement('div');
-    menu1.classList.add('dropdown-menu');
-
-    const item1 = document.createElement('a');
-    item1.innerHTML = `Atencion Y servicio al Cliente Afiliado`;
-    item1.classList.add('dropdown-item');
-    item1.href = 'https://biz.payulatam.com/L0ca71194AA168A';
-
-    const item2 = document.createElement('a');
-    item2.innerHTML = `Atencion Y servicio al Cliente  No Afiliado`;
-    item2.classList.add('dropdown-item');
-    item2.href = 'https://biz.payulatam.com/L0ca7117CADA8E8';
-
-    const item3 = document.createElement('a');
-    item3.innerHTML = `Reglamento Tecnico Afiliado`;
-    item3.classList.add('dropdown-item');
-    item3.href = 'https://biz.payulatam.com/L0ca71109BCDDED';
-
-    const item4 = document.createElement('a');
-    item4.innerHTML = `Reglamento Tecnico No Afiliado`;
-    item4.classList.add('dropdown-item');
-    item4.href = 'https://biz.payulatam.com/L0ca711D2314C40';
-
-    const item5 = document.createElement('a');
-    item5.innerHTML = `Detección de Condiciones Subestándares en EDS Afiliado`;
-    item5.classList.add('dropdown-item');
-    item5.href = 'https://biz.payulatam.com/L0ca71109BCDDED';
-
-    const item6 = document.createElement('a');
-    item6.innerHTML = `Detección de Condiciones Subestándares en EDS NO Afiliado`;
-    item6.classList.add('dropdown-item');
-    item6.href = 'https://biz.payulatam.com/L0ca71109BCDDED';
-
-
-    menu1.appendChild(item1);
-    menu1.appendChild(item2);
-    menu1.appendChild(item3);
-    menu1.appendChild(item4);
-    menu1.appendChild(item5);
-    menu1.appendChild(item6);
-    dropdown1.appendChild(curso);
-    dropdown1.appendChild(menu1);
-    resultadin.appendChild(dropdown1);
-    console.log(resultadin);
- */
 
 }
 
@@ -512,52 +422,3 @@ function limpiarHTML() {
       resultadin.removeChild(resultadin.firstChild);
   }
 }
-
-
-/* let subjectObject = {
-    "Cursos": {
-      "Atencion y Servicio al Cliente": ["Afiliado", "No afiliado"],
-      "Reglamento Tecnico EDS": ["Afiliado", "No afiliado"],
-      "Detección de Condiciones Subestándares en EDS": ["Afiliado", "No afiliado"]    
-    },
-    "Afiliaciones Antioquia": {
-      "PHP": ["Variables", "Strings", "Arrays"],
-      "SQL": ["SELECT", "UPDATE", "DELETE"]
-      
-    },
-    "Afiliaciones Bogotá": {
-        "PHP": ["Variables", "Strings", "Arrays"],
-        "SQL": ["SELECT", "UPDATE", "DELETE"]
-        
-      }
-  }
-  window.onload = function() {
-    var subjectSel = document.getElementById("subject");
-    var topicSel = document.getElementById("topic");
-    var chapterSel = document.getElementById("chapter");
-    for (var x in subjectObject) {
-      subjectSel.options[subjectSel.options.length] = new Option(x, x);
-    }
-    subjectSel.onchange = function() {
-      //empty Chapters- and Topics- dropdowns
-      chapterSel.length = 1;
-      topicSel.length = 1;
-      //display correct values
-      for (var y in subjectObject[this.value]) {
-        topicSel.options[topicSel.options.length] = new Option(y, y);
-      }
-    }
-    topicSel.onchange = function() {
-      //empty Chapters dropdown
-      chapterSel.length = 1;
-      //display correct values
-      var z = subjectObject[subjectSel.value][this.value];
-      for (var i = 0; i < z.length; i++) {
-        chapterSel.options[chapterSel.options.length] = new Option(z[i], z[i]);
-      }
-    }
-  }
-
-
-
- */
